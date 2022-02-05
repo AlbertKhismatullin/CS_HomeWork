5d25ec68776b9e7d3c5d3cf793b5bc60224f68d8
Albert <alberthismatullin02795@gmail.com>
2022-02-05 21:08:34 +0500

    task04

---
+++ task04/Program.cs
@@ -0,0 +1,39 @@
+﻿// По заданному номеру дня недели вывести его название
+
+Console.WriteLine("Введите номер дня недели");
+string number = Console.ReadLine();
+if(number == "1") 
+{
+    Console.Write("День недели: ");
+    Console.WriteLine("Понедельник");
+}
+if(number == "2") 
+{
+    Console.Write("День недели: ");
+    Console.WriteLine("Вторник");
+}
+if(number == "3") 
+{
+    Console.Write("День недели: ");
+    Console.WriteLine("Среда");
+}
+if(number == "4") 
+{
+    Console.Write("День недели: ");
+    Console.WriteLine("Четверг");
+}
+if(number == "5") 
+{
+    Console.Write("День недели: ");
+    Console.WriteLine("Пятница");
+}
+if(number == "6") 
+{
+    Console.Write("День недели: ");
+    Console.WriteLine("Суббота");
+}
+if(number == "7") 
+{
+    Console.Write("День недели: ");
+    Console.WriteLine("Воскресенье");
+}
