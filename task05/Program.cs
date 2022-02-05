3cc2aae60ade8cf7a2137b2ea9cc4e288e8ec316
Albert <alberthismatullin02795@gmail.com>
2022-02-05 20:18:27 +0500

    task05

---
+++ task05/Program.cs
@@ -0,0 +1,14 @@
+﻿//Найти максимальное из трех чисел
+
+int a = 7;
+int b = 4;
+int c = 9;
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.Write("max = ");
+Console.WriteLine(max);
