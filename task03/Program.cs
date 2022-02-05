7278de0fa88adbb522e2221601764adb0ac52a17
Albert <alberthismatullin02795@gmail.com>
2022-02-05 20:34:04 +0500

    task03

---
+++ task03/Program.cs
@@ -0,0 +1,17 @@
+﻿//Даны два числа. Показать большее и меньшее число
+
+int a = 7;
+int b = 5;
+
+int max = a;
+int min = b;
+if (b > a) 
+{
+    max = b;
+    min = a;
+}
+Console.Write("max = ");
+Console.WriteLine(max);
+Console.Write("min = ");
+Console.WriteLine(min);
+
