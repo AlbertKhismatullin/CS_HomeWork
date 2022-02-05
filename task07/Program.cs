9cc69899747146263e2fb1942f8a4ad158708bf0
Albert <alberthismatullin02795@gmail.com>
2022-02-05 22:54:47 +0500

    task07

---
+++ task07/Program.cs
@@ -0,0 +1,10 @@
+﻿//Выяснить является ли число чётным
+
+bool OddOrEven (int a)
+{
+    return a%2==0;
+}
+
+OddOrEven(6);
+if (OddOrEven(6)) Console.WriteLine("Число четное");
+else Console.WriteLine("Число нечетное");
