b07a30f946f89dbadea30dfd62cb8a4c55005f8b
Aльберт Хисматуллин <alberthismatullin02795@gmail.com>
2022-02-11 21:00:28 +0500

    task08

---
+++ task08/Program.cs
@@ -0,0 +1,29 @@
+﻿// Показать числа от -N до N
+
+int x = 0; 
+int y = 0; 
+int res = 0;
+string s = String.Empty;
+
+System.Console.WriteLine("Введите отрицательное число -N");
+s = Console.ReadLine();
+x = Convert.ToInt32(s);
+
+System.Console.WriteLine("Введите положительное число N");
+s = Console.ReadLine();
+y = Convert.ToInt32(s);
+
+res = y - x;
+int [] arr = new int [res+1];
+arr[0]= x;
+arr[res] = y;
+
+for (int i = 1; i < arr.Length-1; i++)
+{
+    arr[i] = arr[i-1] + 1;
+}
+
+for (int j = 0; j < arr.Length; j++)
+{
+    Console.Write(arr[j] + " ");
+}
