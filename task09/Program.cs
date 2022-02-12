9275c0e9d0e393ebabde27f1d442f8de4db91910
Aльберт Хисматуллин <alberthismatullin02795@gmail.com>
2022-02-12 09:40:37 +0500

    task09

---
+++ task09/Program.cs
@@ -0,0 +1,13 @@
+﻿// Показать четные числа от 1 до N
+
+int a = 0;
+
+Console.WriteLine("Введите число N");
+string s = string.Empty;
+s = Console.ReadLine();
+a = Convert.ToInt32(s);
+
+for (int i = 1; i <= a; i++)
+{
+    if(i%2 == 0) Console.Write(i + " ");
+}
