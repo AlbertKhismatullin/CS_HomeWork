0770fac0560e35882b06b1b7f2fa314e97aa8a4a
Aльберт Хисматуллин <alberthismatullin02795@gmail.com>
2022-02-12 09:49:33 +0500

    task10

---
+++ task10/Program.cs
@@ -0,0 +1,13 @@
+﻿// Показать последнюю цифру трёхзначного числа
+
+int a = 0;
+string s = string.Empty;
+
+Console.WriteLine("Введите трехзначное число ");
+s = Console.ReadLine();
+a = s.Length;
+
+if(a == 3) 
+{
+    Console.WriteLine("Последняя цифра трехзначного числа " + s[s.Length-1]);
+}
