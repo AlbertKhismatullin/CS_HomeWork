1b46b5c9c7bd7133357046dc7ee9851117f7c357
Aльберт Хисматуллин <alberthismatullin02795@gmail.com>
2022-02-12 10:11:51 +0500

    task13

---
+++ task13/Program.cs
@@ -0,0 +1,15 @@
+﻿// Удалить вторую цифру трёхзначного числа
+
+int a = 0;
+string s = string.Empty;
+
+Console.WriteLine("Введите трехзначное число ");
+s = Console.ReadLine();
+a = s.Length;
+
+if(a == 3) 
+{
+    Console.WriteLine("Удалить вторую цифру " + s[s.Length-2]); 
+    Console.WriteLine("Остается цифра " + s[s.Length-3] + s[s.Length-1]);
+}
+
