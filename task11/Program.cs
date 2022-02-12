fca3ae49017dbd5e334aff25767cce1f3c3a2286
Aльберт Хисматуллин <alberthismatullin02795@gmail.com>
2022-02-12 09:53:13 +0500

    task11

---
+++ task11/Program.cs
@@ -0,0 +1,13 @@
+﻿// Показать вторую цифру трёхзначного числа
+
+int a = 0;
+string s = string.Empty;
+
+Console.WriteLine("Введите трехзначное число ");
+s = Console.ReadLine();
+a = s.Length;
+
+if(a == 3) 
+{
+    Console.WriteLine("Вторая цифра трехзначного числа " + s[s.Length-2]);
+}
