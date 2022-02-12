ec300b4cccf3b0944efb27600413a321766ab8cb
Aльберт Хисматуллин <alberthismatullin02795@gmail.com>
2022-02-12 10:40:31 +0500

    task15

---
+++ task15/Program.cs
@@ -0,0 +1,17 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+
+int a = 0;
+string s = string.Empty;
+
+Console.WriteLine("Введите любое число ");
+s = Console.ReadLine();
+a = s.Length;
+
+if(a >= 3) 
+{
+    Console.WriteLine("Третья цифра числа " + s[s.Length-2]);
+}
+else
+{
+    Console.WriteLine("Третья цифра числа отсутствует ");
+}
