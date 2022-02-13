bbcd4ada8c2fa0b79cc7a91cb6da56c02ab24267
Aльберт Хисматуллин <alberthismatullin02795@gmail.com>
2022-02-13 10:27:08 +0500

    task25

---
+++ task25/Program.cs
@@ -0,0 +1,14 @@
+﻿//Найти сумму чисел от 1 до А
+
+int a = 0, sum = 0;
+
+System.Console.WriteLine("Введите число A");
+string s = string.Empty;
+s = Console.ReadLine();
+a = Convert.ToInt32(s);
+
+for (int i = 1; i <= a; i++)
+{
+       sum = sum + i;
+}
+System.Console.Write(sum);
