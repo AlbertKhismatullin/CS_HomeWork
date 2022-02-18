c01b0aa26611383cda4f0916f6f3ad6a461478b6
Aльберт Хисматуллин <alberthismatullin02795@gmail.com>
2022-02-18 13:33:44 +0500

    task45

---
+++ task45/Program.cs
@@ -0,0 +1,12 @@
+﻿//Показать числа Фибоначчи
+
+double Fibonacci(double n)
+{
+    if (n == 1 || n == 2) return 1;
+    else return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+
+for (double i = 1; i < 40; i++)
+{
+    System.Console.WriteLine("Число Фибоначчи от {0} равно {1}", i, Fibonacci(i));
+}
