aa8b3e42da72b430e3ddfee8d806384490991db0
Aльберт Хисматуллин <alberthismatullin02795@gmail.com>
2022-02-18 16:52:40 +0500

    task27

---
+++ task27/Program.cs
@@ -0,0 +1,10 @@
+﻿// Определить количество цифр в числе
+
+System.Console.WriteLine("Введите число ");
+string s;
+int count=0;
+s= Console.ReadLine();
+count = s.Length;
+
+
+System.Console.WriteLine("Количество цифр в числе: "+ count);
