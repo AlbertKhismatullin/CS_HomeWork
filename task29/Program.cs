570d4f437430007b5129a9712779e224072a798b
Aльберт Хисматуллин <alberthismatullin02795@gmail.com>
2022-02-19 17:01:55 +0500

    task29

---
+++ task29/Program.cs
@@ -0,0 +1,15 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+
+int N = 0, product = 1;
+string s = string.Empty;
+
+System.Console.WriteLine("Введите число N");
+s = Console.ReadLine();
+N = Convert.ToInt32(s);
+
+for (int i = 1; i <= N; i++)
+{
+    product = product * i;
+}
+
+System.Console.WriteLine("Произведение чисел от 1 до " + N + " равно "+ product);
