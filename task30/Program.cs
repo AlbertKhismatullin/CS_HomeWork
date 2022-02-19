e1eee6241fdf73b842f56f814ea7c8694e4ef2fc
Aльберт Хисматуллин <alberthismatullin02795@gmail.com>
2022-02-19 17:20:15 +0500

    task30

---
+++ task30/Program.cs
@@ -0,0 +1,15 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+
+double a, b;
+Console.WriteLine("Введите число, до которого будут показаны кубы");
+a = Int32.Parse(Console.ReadLine());
+
+for (int i = 1; i <= a; i++)
+{
+    b = Math.Pow(i, 3);
+    if (b % 2 == 0) 
+    {
+        System.Console.WriteLine("куб четного числа " + i + " равен " + b);
+    }
+}
+
