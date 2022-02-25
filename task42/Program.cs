e8e76bc8aabcc0490eeca35112e51541f4b340e7
Aльберт Хисматуллин <alberthismatullin02795@gmail.com>
2022-02-25 20:19:57 +0500

    task42

---
+++ task42/Program.cs
@@ -0,0 +1,14 @@
+﻿// Определить сколько чисел больше 0 введено с клавиатуры
+
+int count=0, a;
+Console.WriteLine("Введите любое число");
+string s = Console.ReadLine();
+a = Convert.ToInt32(s);
+
+while (a>0)
+{
+    if (a%10!=0) count++;
+    a=a/10;
+}
+
+Console.WriteLine("Больше нуля введено чисел "+ count);
