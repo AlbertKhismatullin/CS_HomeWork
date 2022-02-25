4bdd8ea2dc80055544a973d72803d80ac62e5f1a
Aльберт Хисматуллин <alberthismatullin02795@gmail.com>
2022-02-25 21:35:49 +0500

    task49

---
+++ task49/Program.cs
@@ -0,0 +1,15 @@
+﻿//Показать двумерный массив размером m×n заполненный вещественными числами
+
+double[,] a = new double [3,2]{{1.2,2.3}, {3.4,4.5}, {5.6,6.7}};
+
+void Print2D(double[,] a)
+{
+    for (int i = 0; i < a.GetLength(0); i++)
+    {
+        for (int j = 0; j < a.GetLength(1); j++)
+            System.Console.Write($"{a[i,j],6}");
+        System.Console.WriteLine();
+    }
+
+}
+Print2D(a);
