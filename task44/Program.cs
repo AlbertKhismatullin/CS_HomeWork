6ff5c446c744a48946e655f685927e43587c84dc
Aльберт Хисматуллин <alberthismatullin02795@gmail.com>
2022-02-25 20:34:57 +0500

    task44

---
+++ task44/Program.cs
@@ -0,0 +1,10 @@
+﻿/* Найти точку пересечения двух прямых заданных уравнением 
+ y = k1 * x + b1, y = k2 * x + b2, b1 k1 и b2 и k2 заданы*/
+
+void PointAcross (int b1, int k1,int b2,int k2)
+{
+    int x = (b2 - b1) / (k1 - k2);
+    int y = x * k2 + b2;
+    System.Console.WriteLine("Точка пересечения прямых: x = {0} y = {1}" , x, y);
+}
+PointAcross(5,7,2,9);
