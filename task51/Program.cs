c6ecbe014c654ac06087f02ad714e9186abf36d2
Aльберт Хисматуллин <alberthismatullin02795@gmail.com>
2022-02-26 10:32:44 +0500

    task51

--- task51/Program.cs
+++ task51/Program.cs
@@ -12,7 +12,7 @@ for (int i = 0; i < a.GetLength(0); i++)
         for (int j = 0; j < a.GetLength(1); j++)
         {
             a[i,j] = i + j;
-            System.Console.Write($"{a[i,j],4}");
+            System.Console.WriteLine($"{i} | {j} | {a[i,j]}");
         }
            
         System.Console.WriteLine();
