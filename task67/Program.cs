1e4d2af883299939db6b38bfeefbd8351f2b9e6c
Aльберт Хисматуллин <alberthismatullin02795@gmail.com>
2022-03-05 20:39:15 +0500

    task67

---
+++ task67/Program.cs
@@ -0,0 +1,9 @@
+﻿// Показать натуральные числа от N до 1, N задано
+
+string NumbersRec(int a, int N)
+{
+    if (N >= a) return NumbersRec(a + 1, N) + $"{a} ";
+    else return String.Empty;
+}
+
+System.Console.WriteLine(NumbersRec (1, 10));
