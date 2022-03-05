a960e5b1e61de75ae59eae1a826a558de05cbf46
Aльберт Хисматуллин <alberthismatullin02795@gmail.com>
2022-03-05 21:03:06 +0500

    task69

---
+++ task69/Program.cs
@@ -0,0 +1,8 @@
+﻿// Найти сумму элементов от M до N, N и M заданы
+
+int SumRec (int M, int N)
+{
+    if (M == N) return N;
+    else return M + SumRec(M + 1, N);
+}
+System.Console.WriteLine(SumRec(2, 10));
