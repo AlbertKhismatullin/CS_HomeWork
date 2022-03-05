4df4f2ea8173b5f7c7cda929cb8eb8a2d4b513dd
Aльберт Хисматуллин <alberthismatullin02795@gmail.com>
2022-03-05 20:42:47 +0500

    task68

---
+++ task68/Program.cs
@@ -0,0 +1,9 @@
+﻿// Показать натуральные числа от M до N, N и M заданы
+
+string NumbersRec(int M, int N)
+{
+    if (N >= M) return $"{M} " + NumbersRec(M + 1, N);
+    else return String.Empty;
+}
+
+System.Console.WriteLine(NumbersRec (6, 26));
